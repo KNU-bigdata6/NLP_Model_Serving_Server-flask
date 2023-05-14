55d46fdfb9f82c28212f5115fc9e6e2c086dacf7
YJ <jyjin1206@gmail.com>
2023-05-14 20:07:04 +0900

    code modify

--- routes/business_dialogue.py
+++ routes/business_dialogue.py
@@ -2,38 +2,36 @@ from flask import Flask, render_template, request, jsonify, make_response, Bluep
 from transformers import AutoTokenizer
 import torch
 
-business = Blueprint('buisiness', __name__, url_prefix='/business')
+business = Blueprint('business', __name__, url_prefix='/business')
 
 # 모델 불러오기
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 business_model = torch.load('model/business_small_model.pt')
+business_model.eval()   
 
 # Define a dictionary to store chat history for each user
-chat_histories = {}
+business_histories_ids = {}
 
 @business.route('/predict', methods=['POST'])
 def predict():
   user_id = request.form['user_id'] 
   user_input = request.form['user_input'] 
 
-  # Check if user ID exists in chat_histories, if not, create a new chat history
-  if user_id not in chat_histories:
-      chat_histories[user_id] = []
+  # Check if user ID exists in business_histories_ids, if not, create a new chat history
+  if user_id not in business_histories_ids:
+      business_histories_ids[user_id] = []
 
   # Encode user input and add eos_token to create new_user_input_ids
   new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
 
-  # Append new_user_input_ids to chat history for the user
-  chat_histories[user_id].append(new_user_input_ids)
-
   # Concatenate chat history for the user and generate response
-  bot_input_ids = torch.cat(chat_histories[user_id], dim=-1)
+  bot_input_ids = torch.cat([business_histories_ids[user_id], new_user_input_ids], dim=-1) if len(business_histories_ids[user_id]) >0 else new_user_input_ids
   
   # Decode and return bot's response
-  chat_history_ids = business_model.generate(bot_input_ids, max_length=200, pad_token_id=tokenizer.eos_token_id)
-
+  business_histories_ids[user_id] = business_model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+  
   # Decode and return bot's response
-  bot_response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)    
+  bot_response = tokenizer.decode(business_histories_ids[user_id][:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)    
 
   return make_response(bot_response, 200)
 
@@ -41,7 +39,7 @@ def predict():
 @business.route('/', methods=['DELETE'])
 def delete_user_history():
   user_id = request.form['user_id'] 
-  if user_id in chat_histories:
-    chat_histories[user_id].clear()
+  if user_id in business_histories_ids:
+    business_histories_ids[user_id]=[]
   return make_response('', 200)
   
--- routes/empathy_dialogue.py
+++ routes/empathy_dialogue.py
@@ -1,60 +1,45 @@
-"""
-    [모델 개발중]
-"""
-from flask import Flask, render_template, request, jsonify, make_response, Blueprint, redirect, url_for
-from transformers import AutoTokenizer
-import torch
+# from flask import Flask, render_template, request, jsonify, make_response, Blueprint, redirect, url_for
+# from transformers import AutoTokenizer
+# import torch
 
-empathy = Blueprint('empathy', __name__, url_prefix='/empathy')
+# emphathy = Blueprint('emphathy', __name__, url_prefix='/emphathy')
 
-# 모델 불러오기
+# # 모델 불러오기
 # tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-# empathy_model = torch.load('model/model_2c.pt')
-# empathy_model.eval()   
+# emphathy_model = torch.load('model/emphathy_small_model.pt')
+# emphathy_model.eval()   
 
-# Define a dictionary to store chat history for each user
-chat_histories = {}
+# # Define a dictionary to store chat history for each user
+# emphathy_histories_ids = {}
 
-@empathy.route('/predict', methods=['POST'])
-def predict():
-    user_id = request.form['user_id'] 
-    user_input = request.form['user_input'] 
+# @emphathy.route('/predict', methods=['POST'])
+# def predict():
+#   user_id = request.form['user_id'] 
+#   user_input = request.form['user_input'] 
 
-    # Check if user ID exists in chat_histories, if not, create a new chat history
-    if user_id not in chat_histories:
-        chat_histories[user_id] = []
+#   # Check if user ID exists in emphathy_histories_ids, if not, create a new chat history
+#   if user_id not in emphathy_histories_ids:
+#       emphathy_histories_ids[user_id] = []
 
-    # Encode user input and add eos_token to create new_user_input_ids
-    new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
+#   # Encode user input and add eos_token to create new_user_input_ids
+#   new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
 
-    # Append new_user_input_ids to chat history for the user
-    chat_histories[user_id].append(new_user_input_ids)
+#   # Concatenate chat history for the user and generate response
+#   bot_input_ids = torch.cat([emphathy_histories_ids[user_id], new_user_input_ids], dim=-1) if len(emphathy_histories_ids[user_id]) >0 else new_user_input_ids
+  
+#   # Decode and return bot's response
+#   emphathy_histories_ids[user_id] = emphathy_model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+  
+#   # Decode and return bot's response
+#   bot_response = tokenizer.decode(emphathy_histories_ids[user_id][:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)    
 
-    # Concatenate chat history for the user and generate response
-    bot_input_ids = torch.cat(chat_histories[user_id], dim=-1)
-    
-    # Decode and return bot's response
-    chat_history_ids = empathy_model.generate(
-        bot_input_ids, max_length=200,
-        pad_token_id= tokenizer.eos_token_id,  
-        no_repeat_ngram_size=3,       
-        do_sample=True, 
-        top_k=100, 
-        top_p=0.7,
-        temperature=0.8
-    )
+#   return make_response(bot_response, 200)
 
-    # Decode and return bot's response
-    bot_response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)    
-
-    print(chat_histories)
-    return make_response(bot_response, 200)
-
-# 이전 대화 기록 초기화
-@empathy.route('/', methods=['DELETE'])
-def delete_user_history():
-  user_id = request.form['user_id'] 
-  if user_id in chat_histories:
-    chat_histories[user_id].clear()
-  return make_response('', 200)
+# # 이전 대화 기록 초기화
+# @emphathy.route('/', methods=['DELETE'])
+# def delete_user_history():
+#   user_id = request.form['user_id'] 
+#   if user_id in emphathy_histories_ids:
+#     emphathy_histories_ids[user_id]=[]
+#   return make_response('', 200)
   
